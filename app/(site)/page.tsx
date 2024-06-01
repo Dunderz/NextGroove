c835f6596bb8031b9977171f4c159b717b15e2de
Dunderz <isaacknam711@gmail.com>
2024-06-01 16:01:26 -0400

    SidebarItem styles, Library initial styles, Home initial styles

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,10 @@
 import Header from "@/components/Header";
 
 export default function Home() {
-  return <Header></Header>;
+  return (
+    <div className="h-full w-full bg-neutral-900">
+      <p className="text-white">HELLO</p>
+      <Header></Header>
+    </div>
+  );
 }
--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -24,7 +24,7 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
       )}
     >
       <Icon size={20} />
-      <p>{label}</p>
+      <p className="truncate w-full">{label}</p>
     </Link>
   );
 };
