e75b2ca692dd41b38fa942c707dd66a3e64a6aa3
Dunderz <isaacknam711@gmail.com>
2024-06-02 19:42:56 -0400

    button-styles

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,28 @@
 import { twMerge } from "tailwind-merge";
+import { forwardRef } from "react";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const Button = () => {
-  return <button>Button</button>;
-};
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, disabled, type = "button", ...props }, ref) => {
+    return (
+      <button
+        className={twMerge(
+          `w-full rounded-full bg-green-500 border-transparent px-3
+           py-3 disabled:cursor-not-allowed disabled:opacity-50 text-black 
+           font-bold hover:opacity-75 transition`
+        )}
+        type={type}
+        disabled={disabled}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
 
 export default Button;
