8804fac8cc319c185cb85b3905fe8299e918e1fa
Dunderz <isaacknam711@gmail.com>
2024-06-02 12:08:21 -0400

    left and right nav buttons for header

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,8 +3,9 @@ import Header from "@/components/Header";
 export default function Home() {
   return (
     <div className="h-full w-full bg-neutral-900 rounded-lg overflow-hidden overflow-y-auto">
-      <p className="text-white">HELLO</p>
-      <Header></Header>
+      <Header>
+        <h1>THIS IS THE HEADER</h1>
+      </Header>
     </div>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,40 @@
-const Header = () => {
+"use client";
+
+import { twMerge } from "tailwind-merge";
+import { useRouter } from "next/navigation";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+// import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import { FaUserAlt } from "react-icons/fa";
+import { toast } from "react-hot-toast";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
   return (
-    <div>
-      <h1>Header</h1>
+    <div
+      className={twMerge(
+        `h-fit bg-gradient-to-b from-emerald-800 p-6`,
+        className
+      )}
+    >
+      <div className="w-full mb-4 items-center justify-between">
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button className="flex rounded-full bg-black items-center justify-center hover:opacity-75 transition">
+            <RxCaretLeft className="text-white" size={35} />
+          </button>
+          <button className="flex rounded-full bg-black items-center justify-center hover:opacity-75 transition">
+            <RxCaretRight className="text-white" size={35} />
+          </button>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
