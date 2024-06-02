26aa93e7c22b723f79cc8f19eb03e78cc29d5c38
Dunderz <isaacknam711@gmail.com>
2024-06-01 23:01:50 -0400

    added overflow classes to page.tsx, spent time understanding nextjs concepts

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="h-full w-full bg-neutral-900">
+    <div className="h-full w-full bg-neutral-900 rounded-lg overflow-hidden overflow-y-auto">
       <p className="text-white">HELLO</p>
       <Header></Header>
     </div>
