b387bb0840d26c7227f7354f8882d2bfa68e4134
Dunderz <isaacknam711@gmail.com>
2024-06-05 16:19:57 -0400

    AuthModal styles applied

--- hooks/useAuthModal.ts
+++ hooks/useAuthModal.ts
@@ -2,14 +2,14 @@ import { create } from "zustand";
 
 interface AuthModalStore {
   isOpen: boolean;
-  open: () => void;
-  close: () => void;
+  onOpen: () => void;
+  onClose: () => void;
 }
 
 const useAuthModal = create<AuthModalStore>((set) => ({
   isOpen: false,
-  open: () => set({ isOpen: true }),
-  close: () => set({ isOpen: false }),
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
 }));
 
 export default useAuthModal;
