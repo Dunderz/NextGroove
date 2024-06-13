dbf9a0b8f7936b513f14011a252f3d92b807c23d
Dunderz <isaacknam711@gmail.com>
2024-06-13 17:13:56 -0400

    UploadModal imports

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import uniqid from "uniqid";
+import toast from "react-hot-toast";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
+
+import Modal from "./Modal";
+import Button from "./Button";
+import Input from "./Input";
+
 const UploadModal = () => {
   return <div>UploadModal</div>;
 };
