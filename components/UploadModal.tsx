5ad9f11cec3f9aa378a5a2fee90a298343731390
Dunderz <isaacknam711@gmail.com>
2024-06-14 17:17:23 -0400

    uploadModal onSubmit function

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -15,6 +15,70 @@ import Button from "./Button";
 import Input from "./Input";
 
 const UploadModal = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
+  const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+
+  const { register, handleSubmit, reset } = useForm<FieldValues>({
+    defaultValues: {
+      title: "",
+      author: "",
+      song: null,
+      image: null,
+    },
+  });
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      reset();
+      uploadModal.onClose();
+    }
+  };
+
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+    try {
+      setIsLoading(true);
+
+      const imageFile = values.image?.[0];
+      const songFile = values.song?.[0];
+
+      if (!imageFile || !songFile) {
+        toast.error("Missing fields");
+        return;
+      }
+
+      const uniqueID = uniqid();
+
+      // Upload song
+      const { data: songData, error: songError } = await supabaseClient.storage
+        .from("songs")
+        .upload(`song-${values.title}-${uniqueID}`, songFile, {
+          cacheControl: "3600",
+          upsert: false,
+        });
+
+      if (songError) {
+        setIsLoading(false);
+        return toast.error("Failed to upload song");
+      }
+
+      // Upload image
+      const { data: imageData, error: imageError } =
+        await supabaseClient.storage
+          .from("images")
+          .upload(`image-${values.title}-${uniqueID}`, imageFile, {
+            cacheControl: "3600",
+            upsert: false,
+          });
+    } catch (error) {
+      toast.error("Something went wrong.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return <div>UploadModal</div>;
 };
 
