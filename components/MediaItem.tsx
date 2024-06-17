9c55824eccee139eb7e8f7bde0393e47e4099769
Dunderz <isaacknam711@gmail.com>
2024-06-17 19:49:39 -0400

    MediaItem initial styles

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -1,5 +1,42 @@
-const MediaItem = () => {
-  return <div>MediaItem</div>;
+"use client";
+
+import useLoadImage from "@/hooks/useLoadImage";
+// import usePlayer from "@/hooks/usePlayer";
+import { Song } from "@/types";
+import Image from "next/image";
+
+interface MediaItemProps {
+  data: Song;
+  onClick?: (id: string) => void;
+}
+
+const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
+  // const player = usePlayer();
+  const imageUrl = useLoadImage(data);
+
+  const handleClick = () => {
+    if (onClick) {
+      return onClick(data.id);
+    }
+    //return player.play(data);
+  };
+
+  return (
+    <div onClick={handleClick}>
+      <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
+        <Image
+          fill
+          src={imageUrl || "/images/liked.png"}
+          className="object-cover"
+          alt="Media Item"
+        />
+      </div>
+      <div>
+        <p>{data.title}</p>
+        <p>{data.author}</p>
+      </div>
+    </div>
+  );
 };
 
 export default MediaItem;
