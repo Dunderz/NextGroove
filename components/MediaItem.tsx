e4cf8410dcc870b7a4b4b50d27fb124f620a89f6
Dunderz <isaacknam711@gmail.com>
2024-06-26 15:15:21 -0400

    use-sound installed, Player component in layout

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -22,7 +22,10 @@ const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
   };
 
   return (
-    <div onClick={handleClick}>
+    <div
+      onClick={handleClick}
+      className="flex items-center gap-x-3 cursor-pointer hover:bg-neutral-800/50 w-full p-2 rounded-md"
+    >
       <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
         <Image
           fill
@@ -31,9 +34,9 @@ const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
           alt="Media Item"
         />
       </div>
-      <div>
-        <p>{data.title}</p>
-        <p>{data.author}</p>
+      <div className="flex flex-col gap-y-1 overflow-hidden">
+        <p className="text-white truncate">{data.title}</p>
+        <p className="text-neutral-400 text-sm truncate">{data.author}</p>
       </div>
     </div>
   );
