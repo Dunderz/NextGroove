e8a6a06784efe55f42a74a251ea2978a4632e880
Dunderz <isaacknam711@gmail.com>
2024-06-28 13:41:36 -0400

    useDebounce hook

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,1 +1,13 @@
 "use client";
+
+import qs from "query-string";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import Input from "./Input";
+
+const SearchInput = () => {
+  return <div>Search Input</div>;
+};
+
+export default SearchInput;
