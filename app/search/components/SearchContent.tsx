af851a957d2fe6aaad053a560860b1841f6c7636
Dunderz <isaacknam711@gmail.com>
2024-06-28 13:26:14 -0400

    SearchContent and SearchInput initializations

---
+++ app/search/components/SearchContent.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import LikeButton from "@/components/LikeButton";
+import MediaItem from "@/components/MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
+import { Song } from "@/types";
+
+interface SearchContentProps {
+  songs: Song[];
+}
+
+const SearchContent = () => {};
+
+export default SearchContent;
---
+++ app/search/page.tsx
@@ -0,0 +1,16 @@
+import getSongsByTitle from "@/actions/getSongsByTitle";
+import Header from "@/components/Header";
+import SearchInput from "@/components/SearchInput";
+import SearchContent from "./components/SearchContent";
+
+interface SearchProps {
+  searchParams: {
+    title: string;
+  };
+}
+
+export const revalidate = 0;
+
+const Search = () => {};
+
+export default Search;
